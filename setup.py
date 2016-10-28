e50ec9fb46ee63a0b617b4d51197f4fcc2bbc99e
Ben Poor <ben.poor@thisisglobal.com>
2016-10-28 21:51:49 +0100

    updated pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='dabmotsls',
-      version='1.0.1',
+      version='1.0.2',
       description='Plugin to the python-mot library to handle DAB Slideshow encoding as per ETSI TS 101 499',
       author='Ben Poor',
       author_email='ben.poor@thisisglobal.com',
       url='https://github.com/GlobalRadio/python-mot-sls',
-      download_url='https://github.com/GlobalRadio/python-mot-sls/tarball/1.0.1',
-      packages=['mot', 'mot.sls'],
+      download_url='https://github.com/GlobalRadio/python-mot-sls/tarball/1.0.2',
+      packages=['mot.sls'],
       package_dir = {'' : 'src'}
      )
